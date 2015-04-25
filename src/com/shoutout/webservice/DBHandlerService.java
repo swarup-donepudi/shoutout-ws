c4819f5290746c3ff87a1c684ec6b97c39478d1b
swarup.donepudi@gmail.com <swarup.donepudi@gmail.com>
2015-04-25 16:38:55 -0500

    Updated the code to Create a Final JSON objects

--- src/com/shoutout/webservice/DBHandlerService.java
+++ src/com/shoutout/webservice/DBHandlerService.java
@@ -3,13 +3,14 @@ package com.shoutout.webservice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,16 +18,20 @@ import org.json.JSONObject;
 @Path("/audioclips")
 public class DBHandlerService {
 	Connection conn = null;
-	
+
 	@GET
 	@Path("/clip-id={param}")
 	public Response getAudioClips(@PathParam("param") String msg){
+		JSONObject finalJSON = new JSONObject();//This will store allClips
+		JSONArray allClips = new JSONArray();//Collection of single clips
+		JSONObject singleClip = null;//JSON Object to store single clip
+		
 		Statement statement = null;
 		ResultSet resultSet = null;
+		int clipId=0;
 		String clipTitle=null;
-		//create Json Object
-		JSONObject json = new JSONObject();
-		String returnString=null;
+		String clipTags=null;
+		String clipCategory=null;
 		try{
 			// This will load the MySQL driver, each DB has its own driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -34,33 +39,36 @@ public class DBHandlerService {
 			conn = DriverManager.getConnection("jdbc:mysql://www.it354.com/it354_keschae?" +
 					"user=it354_keschae&password=steinway");
 			statement = conn.createStatement();
-		      // Result set get the result of the SQL query
-		      resultSet = statement.executeQuery("select * from audio_clips");
-			// PreparedStatements can use variables and are more efficient
-			//preparedStatement = conn.prepareStatement("select * from audio_clips limit 1");
-			// Parameters start with 1
-			//preparedStatement.setString(1, "Test");
-			//preparedStatement.setString(2, "TestEmail");
-		      int i=0;
-		      while(resultSet.next()){
-		    	  i++;
-		    	  clipTitle = resultSet.getString("clipTitle");
-		    	  json.put("ClipTitle"+i,clipTitle);
-		      }
+			// Result set get the result of the SQL query
+			resultSet = statement.executeQuery("select * from audio_clips");
+			
+			while(resultSet.next()){
+				singleClip = new JSONObject();
+				clipId = resultSet.getInt("clipId");
+				clipTitle = resultSet.getString("clipTitle");
+				clipTags = resultSet.getString("clipTags");
+				clipCategory = resultSet.getString("clipCategory");
+				singleClip.put("ClipId",clipId);
+				singleClip.put("ClipTitle",clipTitle);
+				singleClip.put("ClipTags",clipTags);
+				singleClip.put("ClipCategory",clipCategory);
+				allClips.put(singleClip);
+			}
+			finalJSON.put("AllClips",allClips);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 
 		// put some value pairs into the JSON object .
 		try {
-			
-			
+
+
 		} catch (JSONException e2) {
 			e2.printStackTrace();
 		}
 		// finally output the json string       
-		returnString = json.toString();
+		String returnString = finalJSON.toString();
 		return Response.status(200).entity(returnString).build();
 	}
 }
