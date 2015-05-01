26bcc2b195478372c583c5d64d230a3d9b880384
swarup.donepudi@gmail.com <swarup.donepudi@gmail.com>
2015-04-30 19:28:36 -0500

    Added a test case

---
+++ src/test/shoutout/webservice/TestDBConnection.java
@@ -0,0 +1,18 @@
+package test.shoutout.webservice;
+
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestDBConnection {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testPrintHelloWorld() {
+ 
+		Assert.assertEquals("Hello World", "Hello World");
+ 
+	}
+
+}
